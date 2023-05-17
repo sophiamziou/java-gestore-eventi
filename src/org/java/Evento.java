f8ea4ebe74efa147b29d3173973ea16baa1df8f5
sophiamziou <sofiamziou@gmail.com>
2023-05-17 16:16:42 +0200

    milestone 1 done,started mlstn 2

--- src/org/java/Evento.java
+++ src/org/java/Evento.java
@@ -1,6 +1,8 @@
 package org.java;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Evento {
     private String title;
@@ -8,7 +10,7 @@ public class Evento {
     private int totSeats;
     private int resSeats;
     
-	public Evento(String title, LocalDate date, int totSeats) {
+	public Evento(String title, LocalDate date, int totSeats) throws Exception {
 		super();
 		setTitle(title);
 		setDate(date);
@@ -19,35 +21,70 @@ public class Evento {
 	public String getTitle() {
 		return title;
 	}
-	public void setTitle(String title) {
+	public void setTitle(String title) throws Exception {
+		if (title.isEmpty()) {
+			throw new Exception("il titolo è obbligatorio");
+		}
 		this.title = title;
 	}
 	public LocalDate getDate() {
 		return date;
 	}
-	public void setDate(LocalDate date) {
-		this.date = date;
+	public void setDate(LocalDate date) throws Exception {
+		
+		LocalDate currentDate = LocalDate.now();
+		
+		if (date.isAfter(currentDate)) {
+			this.date = date;
+		}else {
+			throw new Exception("Pick a future date!");
+		}
 	}
 	public int getTotSeats() {
 		return totSeats;
 	}
-	private void setTotSeats(int totSeats) {
+	private void setTotSeats(int totSeats) throws Exception {
+		
+		if (totSeats <= 0) {
+			throw new Exception("The number of total seats must be at least 1!");
+		}
 		this.totSeats = totSeats;
 	}
+	
 	public int getResSeats() {
 		return resSeats;
 	}
 	
-	private void reserve(int resSeats) throws Exception {
+	public void reserve(int resSeats, LocalDate date) throws Exception {
+		
+		LocalDate currentDate = LocalDate.now();
 		this.resSeats = getResSeats() + 1;
-		if (getResSeats()>= getTotSeats()) {
+		if (getResSeats() >= getTotSeats()) {
 			throw new Exception("No more seats available!");
 		}
+		if (date.isAfter(currentDate)) {
+			this.date = date;
+		}else {
+			throw new Exception("Pick a future date!");
+		}
 	}
-	private void cancel(int resSeats) throws Exception {
+	
+	public void cancel(int resSeats, LocalDate date) throws Exception {
+		
+		LocalDate currentDate = LocalDate.now();
 		this.resSeats = getResSeats() - 1;
 		if (getResSeats() == 0) {
 			throw new Exception("No reservation to cancel!");
 		}
+		if (date.isAfter(currentDate)) {
+			this.date = date;
+		}else {
+			throw new Exception("Pick a future date!");
+		}
+	}
+	
+	@Override
+	public String toString() {
+        return date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " - " + title;
 	}
 }
--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -1,10 +1,44 @@
 package org.java;
 
-import org.java.Evento;
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-//		LocalDate data = LocalDate.parse(sc.nextLine());
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Set the title of the event");
+		String title = sc.next();
+		
+		LocalDate date = null;
+		
+		while (true) {
+			System.out.println("Set the date of the event in format yyyy-mm-dd");
+			String dateString = sc.next();
+			try {
+				date = LocalDate.parse(dateString);
+				break;
+			} catch (Exception e) {
+				System.out.println("Error : write date in correct format");
+				continue;
+			}
+		}
+		
+		System.out.println("Set the total seats available");
+		int totSeats = sc.nextInt();
+
+		System.out.println("--------------------");
+		
+		try {
+			
+			Evento e1 = new Evento(title, date, totSeats);
+			System.out.println(e1);
+			
+		} catch (Exception e) {
+			
+			System.err.println("Error " + e.getMessage());
+		}
+		
 	}
 }
